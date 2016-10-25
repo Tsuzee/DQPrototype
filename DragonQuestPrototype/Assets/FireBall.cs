81d647b092da299a0b399405178f6b904fb5da47
Sravan Karuturi <knight2356@gmail.com>
2016-10-25 18:14:21 -0400

    Enemy firing added

---
+++ DragonQuestPrototype/Assets/FireBall.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireBall : MonoBehaviour {
+
+//	public Vector2 intialVelocity;
+
+	private Vector2 initialVelocity = new Vector2(5, -1);
+
+	public float projectileDestroyTimer = .5f;
+	private float currentTimer = 0.0f;
+	private Rigidbody2D body2D;
+
+	void Awake(){
+//		body2D = GetComponent<Rigidbody2D>;
+		body2D = GetComponent<Rigidbody2D>();
+	}
+
+	// Use this for initialization
+	void Start () {
+//		float startex = initialVelocity.x * transform.localScale.x;
+//		body2D.velocity = new Vector2 (startex, initialVelocity.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentTimer > projectileDestroyTimer) {
+			Destroy (gameObject);
+		} else {
+			currentTimer += Time.deltaTime;
+		}
+	}
+}
---
+++ DragonQuestPrototype/Assets/ShootProjectile.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootProjectile : MonoBehaviour {
+
+	public float shootDelay = 1f;
+	public GameObject projectilePrefab;
+
+	private float timeElapsed = 0f;
+	private Vector2 initialVelocity = new Vector2(5, -1);
+
+	// Use this for initialization
+	void Start(){
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		Debug.Log (projectilePrefab);
+		if (projectilePrefab != null) {
+			if ( timeElapsed > shootDelay ){
+				Vector2 velocity = new Vector2(10f, 1f);
+				CreateProjectile(transform.position);
+				timeElapsed = 0f;
+			}
+
+			timeElapsed += Time.deltaTime;
+		}
+	}
+
+	public void CreateProjectile(Vector2 pos){
+
+		GameObject clone = Instantiate(projectilePrefab, pos, Quaternion.identity) as GameObject;
+		clone.transform.localScale = 0.3f * transform.localScale;
+		Rigidbody2D body2D = clone.GetComponent<Rigidbody2D> ();
+
+		float startex;
+		startex = -1.0f * initialVelocity.x * transform.localScale.x;
+		Debug.Log (startex);
+		body2D.velocity = new Vector2 (startex, initialVelocity.y);
+	}
+}
